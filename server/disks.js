173743f4e861576228965ffdb4461833cbe73792
Gustas Zilinskas <sunus.bitaitis@gmail.com>
2021-01-29 15:41:04 +0200

    Async version of Disk Usage function

--- server/disks.js
+++ server/disks.js
@@ -1,29 +1,29 @@
-const { exec } = require('child_process');
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec);
 
 const config = require('./config.local.json');
 
-function getDiskUsage(disks) {
+async function getDiskUsage(disks) {
+
   const diskFiles = disks.map(disk => disk.file);
 
-  const diskLines = exec(['df --output=file,used,avail', ...diskFiles].join(' '));
+  const { stdout } = await exec(['df --output=file,used,avail', ...diskFiles].join(' '));
 
-  diskLines.stdout.on('data', (stdout) => {
-    const diskStats = stdout.split('\n').slice(1, -1).map(line => {
-      let [file, used, avail] = line.split(/\s+/);
+  const diskStats = stdout.split('\n').slice(1, -1).map(line => {
+    let [file, used, avail] = line.split(/\s+/);
 
-      // implicit conversion to number seems to be the fastest
-      used = +used;
-      avail = +avail;
+    // implicit conversion to number seems to be the fastest
+    used = +used;
+    avail = +avail;
 
-      return ({
-        file: file,
-        size: used + avail,
-        free: avail,
-      });
+    return ({
+      file: file,
+      size: used + avail,
+      free: avail,
     });
-
-    console.log(diskStats);
   });
+
+  return diskStats;
 }
 
-getDiskUsage(config.disks);
+getDiskUsage(config.disks).then(diskStats => console.log(diskStats));
