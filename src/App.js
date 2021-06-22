40a9dace91b99685c09a9deb5fa0d7a2b2c49cc8
Gustas Zilinskas <sunus.bitaitis@gmail.com>
2021-06-22 16:52:30 +0300

    Minor cleanup of section styles

--- src/App.js
+++ src/App.js
@@ -123,16 +123,16 @@ const Section = props => {
   } = props;
 
   return (
-      <Flex direction='column' grow={grow ? 1 : 0} shrink={grow ? 1 : 0} mb={4}>
+      <Flex direction='column' grow={grow ? 1 : null} mb={4}>
         <Flex justify='space-between'>
-          <Heading as='h1' pb={1} fontSize='4xl'>{title}</Heading>
+          <Heading as='h1' mb={1} fontSize='4xl'>{title}</Heading>
           <Box>
             { (typeof onRefresh === 'function') && (
               <Button colorScheme='teal' size='sm' onClick={onRefresh} isLoading={isLoading}>Refresh</Button>
             ) }
           </Box>
         </Flex>
-        <Box position='relative' flexGrow={grow ? 1 : 0}>{children}</Box>
+        <Box position='relative' flexGrow={grow ? 1 : null}>{children}</Box>
       </Flex>
   );
 }
@@ -193,12 +193,12 @@ const App = () => {
 
   return (
     <Flex direction='column' pos='fixed' w='100%' h='100vh' p={8}>
-      <Section title='Disks' isLoading={du.status === 'pending'} onRefresh={du.execute}>
+      <Section title='Disks' isLoading={du.status === 'pending'} onRefresh={refreshDisks}>
         <Flex>
           {du.value && du.value.map(disk => <DiskItem key={disk.path} stats={disk} />)}
         </Flex>
       </Section>
-      <Section title='Torrents' isLoading={torrents.status === 'pending'} onRefresh={torrents.execute} grow>
+      <Section title='Torrents' isLoading={torrents.status === 'pending'} onRefresh={refreshTorrents} grow>
         {torrents.value && (
           <TorrentsTable data={torrents.value} refresh={refreshAll} />
         )}
