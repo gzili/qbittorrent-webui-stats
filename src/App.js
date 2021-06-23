47ca2c63ca79e53f051492902c7580dd64205465
Gustas Zilinskas <sunus.bitaitis@gmail.com>
2021-06-23 10:26:23 +0300

    Extract useAsync into hooks module

--- src/App.js
+++ src/App.js
@@ -1,47 +1,12 @@
-import { useEffect, useCallback, useState } from 'react';
-import { Box, Flex, Text, Heading, Button } from '@chakra-ui/react';
+import { useCallback } from 'react';
 import moment from 'moment';
+import { Box, Flex, Text, Heading, Button } from '@chakra-ui/react';
 
+import { useAsync } from './hooks';
 import { formatBytes, getTorrentStatsByDay } from './utils';
 import TorrentsTable from './TorrentsTable';
 
 import './App.css';
-
-const useAsync = (asyncFunction, immediate = true) => {
-  const [status, setStatus] = useState('idle');
-  const [value, setValue] = useState(null);
-  const [error, setError] = useState(null);
-
-  // The execute function wraps asyncFunction and
-  // handles setting state for pending, value, and error.
-  // useCallback ensures the below useEffect is not called
-  // on every render, but only if asyncFunction changes.
-  const execute = useCallback(() => {
-    setStatus('pending');
-    //setValue(null);
-    setError(null);
-    return asyncFunction()
-      .then((response) => {
-        setValue(response);
-        setStatus('success');
-      })
-      .catch((error) => {
-        setError(error);
-        setStatus('error');
-      });
-  }, [asyncFunction]);
-
-  // Call execute if we want to fire it right away.
-  // Otherwise execute can be called later, such as
-  // in an onClick handler.
-  useEffect(() => {
-    if (immediate) {
-      execute();
-    }
-  }, [execute, immediate]);
-
-  return { execute, status, value, error };
-};
 
 function DiskItem(props) {
   let { path, size, used, free } = props.stats;
